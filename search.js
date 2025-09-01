0fd1c7ef0ae7160bcf8cd0ced934162050b214c2
hemanthg97 <hemanthg97@gmail.com>
2025-09-01 23:19:12 +0530

    Delete search.js

--- search.js
+++
@@ -1,6 +0,0 @@
-async function askAI() {
-  const query = document.getElementById('query').value;
-  const res = await fetch(`/api/ask?query=${encodeURIComponent(query)}`);
-  const data = await res.json();
-  document.getElementById('results').innerText = data.answer || "No answer";
-}
